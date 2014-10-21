55a1ab3fec5fdf2a081299680fcb4e82f0f0fb0d
Nutil <luis.rpfigueiredo@gmail.com>
2014-10-21 22:40:50 +0100

    Data
    meh. continua merda

--- AEDA projeto HOTEL/AEDA projeto HOTEL/Data.cpp
+++ AEDA projeto HOTEL/AEDA projeto HOTEL/Data.cpp
@@ -12,13 +12,20 @@ Data::Data(int dia, int mes, int ano){
 
 int Data::dateToMinutes(Data date){
 	int res = 0;
-	
-	else if (mes)
+	int temp1, temp2, temp3;
+
+	temp1 = ano * 365 * 24 * 60;//numero de minutos nos anos
+	temp2 = dia * 
+
 
 
 		return res;
 }
 int Data::dateDifferenceS(Data date1, Data date2){
 	int res = 0;
-	return res;
+	int anodiff = date2.ano - date1.ano;
+	int mesdiff = date2.mes - date1.mes;
+	int diadiff = date2.dia - date1.dia;
+
+
 }
